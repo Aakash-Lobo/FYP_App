b01c2aac4525dc41b2d560fcc2b77d80395fef9f
Aakash <aakashplobo@gmail.com>
2023-11-21 01:19:36 +0530

    completed lib dashboard

--- lib/Modules/Librarian/LibrarianSettingsPage.dart
+++ lib/Modules/Librarian/LibrarianSettingsPage.dart
@@ -1,9 +1,265 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-class LibrarianSettingsPage extends StatelessWidget {
+class Penalty {
+  final int penaltyId;
+  int penaltyAmount;
+
+  Penalty({
+    required this.penaltyId,
+    required this.penaltyAmount,
+  });
+
+  factory Penalty.fromJson(Map<String, dynamic> json) {
+    return Penalty(
+      penaltyId: int.parse(json['penalty_id'].toString()),
+      penaltyAmount: int.parse(json['penalty_amount'].toString()),
+    );
+  }
+}
+
+class AllowedDays {
+  final int allowedDaysId;
+  int noOfDays;
+
+  AllowedDays({
+    required this.allowedDaysId,
+    required this.noOfDays,
+  });
+
+  factory AllowedDays.fromJson(Map<String, dynamic> json) {
+    return AllowedDays(
+      allowedDaysId: int.parse(json['allowed_days_id'].toString()),
+      noOfDays: int.parse(json['no_of_days'].toString()),
+    );
+  }
+}
+
+class AllowedBooks {
+  final int allowedBooksId;
+  int qnttyBooks;
+
+  AllowedBooks({
+    required this.allowedBooksId,
+    required this.qnttyBooks,
+  });
+
+  factory AllowedBooks.fromJson(Map<String, dynamic> json) {
+    return AllowedBooks(
+      allowedBooksId: json['allowed_books_id'] ?? 0,
+      qnttyBooks: int.tryParse(json['qntty_books'].toString()) ?? 0,
+    );
+  }
+}
+
+class LibrarianSettingsPage extends StatefulWidget {
   final String username;
 
   LibrarianSettingsPage({required this.username});
+
+  @override
+  _LibrarianSettingsPageState createState() => _LibrarianSettingsPageState();
+}
+
+class _LibrarianSettingsPageState extends State<LibrarianSettingsPage> {
+  late List<Penalty> penalties = [];
+  late List<AllowedDays> allowedDays = [];
+  late List<AllowedBooks> allowedBooks = []; // Initialize with an empty list
+
+  TextEditingController penaltyController = TextEditingController();
+  TextEditingController allowedDaysController = TextEditingController();
+  TextEditingController allowedBooksController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    fetchData();
+  }
+
+  Future<void> fetchData() async {
+    await fetchPenalties();
+    await fetchAllowedDays();
+    await fetchAllowedBooks();
+  }
+
+  Future<void> fetchAllowedDays() async {
+    try {
+      final response = await http.get(
+          Uri.parse('http://localhost/fyp/app/modules/lib/allowed_days.php'));
+      if (response.statusCode == 200) {
+        final dynamic data = json.decode(response.body);
+        final List<dynamic> allowedDaysData = data['data'];
+        setState(() {
+          allowedDays = allowedDaysData
+              .map((allowedDays) => AllowedDays.fromJson(allowedDays))
+              .toList();
+        });
+      } else {
+        print(
+            'Failed to load allowed days. Status code: ${response.statusCode}');
+      }
+    } catch (error) {
+      print('Error fetching allowed days: $error');
+    }
+  }
+
+  Future<void> fetchAllowedBooks() async {
+    try {
+      final response = await http.get(
+          Uri.parse('http://localhost/fyp/app/modules/lib/allowed_books.php'));
+      if (response.statusCode == 200) {
+        final dynamic data = json.decode(response.body);
+        final List<dynamic> allowedBooksData = data['data'];
+        setState(() {
+          allowedBooks = allowedBooksData
+              .map((allowedBooks) => AllowedBooks.fromJson(allowedBooks))
+              .toList();
+        });
+      } else {
+        print(
+            'Failed to load allowed books. Status code: ${response.statusCode}');
+      }
+    } catch (error) {
+      print('Error fetching allowed books: $error');
+    }
+  }
+
+  Future<void> fetchPenalties() async {
+    try {
+      final response = await http
+          .get(Uri.parse('http://localhost/fyp/app/modules/lib/penalties.php'));
+      if (response.statusCode == 200) {
+        final dynamic data = json.decode(response.body);
+        final List<dynamic> penaltiesData = data['data'];
+        setState(() {
+          penalties = penaltiesData
+              .map((penalty) => Penalty.fromJson(penalty))
+              .toList();
+        });
+      } else {
+        print('Failed to load penalties. Status code: ${response.statusCode}');
+      }
+    } catch (error) {
+      print('Error fetching penalties: $error');
+    }
+  }
+
+  Future<void> updatePenalty(int index) async {
+    final response = await http.post(
+      Uri.parse('http://localhost/fyp/app/modules/lib/update_penalty.php'),
+      body: {
+        'penalty_id': penalties[index].penaltyId.toString(),
+        'penalty_amount': penaltyController.text,
+      },
+    );
+
+    if (response.statusCode == 200) {
+      fetchPenalties();
+      Navigator.of(context).pop(); // Close the bottom modal sheet
+    } else {
+      print('Failed to update penalty. Status code: ${response.statusCode}');
+    }
+  }
+
+  Future<void> updateAllowedDays(int index) async {
+    final response = await http.post(
+      Uri.parse('http://localhost/fyp/app/modules/lib/update_allowed_days.php'),
+      body: {
+        'allowed_days_id': allowedDays[index].allowedDaysId.toString(),
+        'no_of_days': allowedDaysController.text,
+      },
+    );
+
+    if (response.statusCode == 200) {
+      fetchAllowedDays();
+      Navigator.of(context).pop(); // Close the bottom modal sheet
+    } else {
+      print(
+          'Failed to update allowed days. Status code: ${response.statusCode}');
+    }
+  }
+
+  Future<void> updateAllowedBooks(int index) async {
+    final response = await http.post(
+      Uri.parse(
+          'http://localhost/fyp/app/modules/lib/update_allowed_books.php'),
+      body: {
+        'allowed_books_id': allowedBooks[index].allowedBooksId.toString(),
+        'qntty_books': allowedBooksController.text,
+      },
+    );
+
+    if (response.statusCode == 200) {
+      fetchAllowedBooks();
+      Navigator.of(context).pop(); // Close the bottom modal sheet
+    } else {
+      print(
+          'Failed to update allowed books. Status code: ${response.statusCode}');
+    }
+  }
+
+  void _showEditModal(BuildContext context, String fieldType, int index) {
+    TextEditingController controller;
+
+    switch (fieldType) {
+      case 'Penalty':
+        controller = penaltyController;
+        break;
+      case 'Allowed Days':
+        controller = allowedDaysController;
+        break;
+      case 'Allowed Books':
+        controller = allowedBooksController;
+        break;
+      default:
+        controller = TextEditingController(); // Default controller
+        break;
+    }
+
+    showModalBottomSheet(
+      context: context,
+      builder: (BuildContext context) {
+        return Padding(
+          padding: EdgeInsets.all(16),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                'Edit $fieldType',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 16),
+              TextField(
+                controller: controller,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(labelText: 'Enter new value'),
+              ),
+              SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: () {
+                  switch (fieldType) {
+                    case 'Penalty':
+                      updatePenalty(index);
+                      break;
+                    case 'Allowed Days':
+                      updateAllowedDays(index);
+                      break;
+                    case 'Allowed Books':
+                      updateAllowedBooks(index);
+                      break;
+                    default:
+                      break;
+                  }
+                },
+                child: Text('Update'),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,9 +267,110 @@ class LibrarianSettingsPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Settings'),
       ),
-      body: Center(
-        child: Text('Dummy'), // Replace with your content
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: <Widget>[
+            SizedBox(height: 20),
+            buildSectionTitle('Penalty'),
+            buildPenaltyTable(),
+            SizedBox(height: 20),
+            buildSectionTitle('Allowed Days'),
+            buildAllowedDaysTable(),
+            SizedBox(height: 20),
+            buildSectionTitle('Allowed Books'),
+            buildAllowedBooksTable(),
+          ],
+        ),
       ),
+    );
+  }
+
+  Widget buildSectionTitle(String title) {
+    return Text(
+      title,
+      style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+    );
+  }
+
+  Widget buildPenaltyTable() {
+    return DataTable(
+      columns: [
+        DataColumn(label: Text('Amount')),
+        DataColumn(label: Text('Actions')),
+      ],
+      rows: penalties.asMap().entries.map((entry) {
+        final index = entry.key;
+        final Penalty penalty = entry.value;
+        return DataRow(
+          cells: [
+            DataCell(Text(penalty.penaltyAmount.toString())),
+            DataCell(
+              ElevatedButton(
+                onPressed: () {
+                  penaltyController.text = penalty.penaltyAmount.toString();
+                  _showEditModal(context, 'Penalty', index);
+                },
+                child: Text('Edit'),
+              ),
+            ),
+          ],
+        );
+      }).toList(),
+    );
+  }
+
+  Widget buildAllowedDaysTable() {
+    return DataTable(
+      columns: [
+        DataColumn(label: Text('Number of Days')),
+        DataColumn(label: Text('Actions')),
+      ],
+      rows: allowedDays.asMap().entries.map((entry) {
+        final index = entry.key;
+        final AllowedDays days = entry.value;
+        return DataRow(
+          cells: [
+            DataCell(Text(days.noOfDays.toString())),
+            DataCell(
+              ElevatedButton(
+                onPressed: () {
+                  allowedDaysController.text = days.noOfDays.toString();
+                  _showEditModal(context, 'Allowed Days', index);
+                },
+                child: Text('Edit'),
+              ),
+            ),
+          ],
+        );
+      }).toList(),
+    );
+  }
+
+  Widget buildAllowedBooksTable() {
+    return DataTable(
+      columns: [
+        DataColumn(label: Text('Quantity')),
+        DataColumn(label: Text('Actions')),
+      ],
+      rows: allowedBooks.asMap().entries.map((entry) {
+        final index = entry.key;
+        final AllowedBooks books = entry.value;
+        return DataRow(
+          cells: [
+            DataCell(Text(books.qnttyBooks.toString())),
+            DataCell(
+              ElevatedButton(
+                onPressed: () {
+                  allowedBooksController.text = books.qnttyBooks.toString();
+                  _showEditModal(context, 'Allowed Books', index);
+                },
+                child: Text('Edit'),
+              ),
+            ),
+          ],
+        );
+      }).toList(),
     );
   }
 }
--- lib/Modules/Librarian/ReturnedBooksPage.dart
+++ lib/Modules/Librarian/ReturnedBooksPage.dart
@@ -1,9 +1,92 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:intl/intl.dart';
 
-class ReturnedBooksPage extends StatelessWidget {
+class ReturnedBook {
+  final String? bookBarcode;
+  final String? borrowerName;
+  final String? bookTitle;
+  final String? dateBorrowed;
+  final String? dueDate;
+  final String? dateReturned;
+  final String? penalty;
+
+  ReturnedBook({
+    required this.bookBarcode,
+    required this.borrowerName,
+    required this.bookTitle,
+    required this.dateBorrowed,
+    required this.dueDate,
+    required this.dateReturned,
+    required this.penalty,
+  });
+
+  factory ReturnedBook.fromJson(Map<String, dynamic> json) {
+    return ReturnedBook(
+      bookBarcode: json['book_barcode'],
+      borrowerName: '${json['firstname']} ${json['lastname']}',
+      bookTitle: json['book_title'],
+      dateBorrowed: json['date_borrowed'],
+      dueDate: json['due_date'],
+      dateReturned: json['date_returned'],
+      penalty: json['book_penalty'],
+    );
+  }
+}
+
+class ReturnedBooksPage extends StatefulWidget {
   final String username;
-
   ReturnedBooksPage({required this.username});
+
+  @override
+  _ReturnedBooksPageState createState() => _ReturnedBooksPageState();
+}
+
+class _ReturnedBooksPageState extends State<ReturnedBooksPage> {
+  List<ReturnedBook> returnedBooks = [];
+  String? dateFrom;
+  String? dateTo;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchReturnedBooks();
+  }
+
+  Future<void> fetchReturnedBooks() async {
+    try {
+      String apiUrl =
+          "http://localhost/fyp/app/modules/lib/viewreturnedbooks.php";
+
+      if (dateFrom != null && dateTo != null) {
+        apiUrl += "?dateFrom=$dateFrom&dateTo=$dateTo";
+      }
+
+      final response = await http.get(Uri.parse(apiUrl));
+
+      if (response.statusCode == 200) {
+        List<dynamic> data = json.decode(response.body);
+
+        // Check if data is not null before mapping
+        if (data != null) {
+          setState(() {
+            returnedBooks =
+                data.map((book) => ReturnedBook.fromJson(book)).toList();
+          });
+        }
+      } else {
+        print('HTTP request failed with status: ${response.statusCode}');
+        print('Response body: ${response.body}');
+      }
+    } catch (e) {
+      print('Error during HTTP request: $e');
+    }
+  }
+
+  void updateDateFilters() {
+    fetchReturnedBooks();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,8 +94,95 @@ class ReturnedBooksPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Returned Books'),
       ),
-      body: Center(
-        child: Text('Dummy'), // Replace with your content
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              'Returned Books Page',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 20),
+            // Date Filters
+            Row(
+              children: [
+                Text('From:'),
+                SizedBox(width: 8),
+                ElevatedButton(
+                  onPressed: () async {
+                    final DateTime? picked = await showDatePicker(
+                      context: context,
+                      initialDate: DateTime.now(),
+                      firstDate: DateTime(2020),
+                      lastDate: DateTime(2101),
+                    );
+
+                    if (picked != null && picked != DateTime.now()) {
+                      setState(() {
+                        dateFrom = DateFormat('yyyy-MM-dd').format(picked!);
+                      });
+                    }
+
+                    updateDateFilters();
+                  },
+                  child: Text('Select Date'),
+                ),
+                SizedBox(width: 16),
+                Text('To:'),
+                SizedBox(width: 8),
+                ElevatedButton(
+                  onPressed: () async {
+                    final DateTime? picked = await showDatePicker(
+                      context: context,
+                      initialDate: DateTime.now(),
+                      firstDate: DateTime(2020),
+                      lastDate: DateTime(2101),
+                    );
+
+                    if (picked != null) {
+                      setState(() {
+                        dateTo = DateFormat('yyyy-MM-dd').format(picked!);
+                      });
+                    }
+
+                    updateDateFilters();
+                  },
+                  child: Text('Select Date'),
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            // Display Returned Books
+            Expanded(
+              child: DataTable(
+                columns: [
+                  DataColumn(label: Text('Barcode')),
+                  DataColumn(label: Text('Borrower')),
+                  DataColumn(label: Text('Title')),
+                  DataColumn(label: Text('Date Borrowed')),
+                  DataColumn(label: Text('Due Date')),
+                  DataColumn(label: Text('Date Returned')),
+                  DataColumn(label: Text('Penalty')),
+                ],
+                rows: returnedBooks.map((book) {
+                  return DataRow(
+                    cells: [
+                      DataCell(Text(book.bookBarcode ?? 'Unknown Barcode')),
+                      DataCell(Text(book.borrowerName ?? 'Unknown Borrower')),
+                      DataCell(Text(book.bookTitle ?? 'Unknown Title')),
+                      DataCell(
+                          Text(book.dateBorrowed ?? 'Unknown Date Borrowed')),
+                      DataCell(Text(book.dueDate ?? 'Unknown Due Date')),
+                      DataCell(Text(book.dateReturned ?? 'Not Returned')),
+                      DataCell(Text(book.penalty ?? 'Unknown Penalty')),
+                    ],
+                  );
+                }).toList(),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
